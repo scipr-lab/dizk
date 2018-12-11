7afd0becf68e83f3f6a25f4514cb4d681d6151d9
Benjamin Smith <bh2smith@users.noreply.github.com>
2018-12-11 17:26:58 +0100

    [HOTFIX] whoops
    Exception should be thrown inside else clause (when app name doesn't exist)

--- src/main/java/profiler/utils/SparkUtils.java
+++ src/main/java/profiler/utils/SparkUtils.java
@@ -51,9 +51,10 @@ public class SparkUtils {
             return "SparkZKSNARKLarge";
         } else if (s.equals("vmsm-sorted-g1")) {
             return "SparkVMSMSortedG1";
+        } else {
+            String exception = String.format("APP parameter '%s' not listed for profiling", s);
+            throw new IllegalArgumentException(exception);
         }
-        String exception = String.format("APP parameter '%s' not listed for profiling", s);
-        throw new IllegalArgumentException(exception);
     }
 
     public static Class[] zksparkClasses() {
