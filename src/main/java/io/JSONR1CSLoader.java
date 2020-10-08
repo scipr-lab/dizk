7a0513a5fcb8bbfc14f11a8687213072b2604c7b
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-10-08 18:38:11 +0100

    Implemented linearComination and Constraints loading

--- src/main/java/io/JSONR1CSLoader.java
+++ src/main/java/io/JSONR1CSLoader.java
@@ -12,6 +12,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.apache.arrow.memory.BaseAllocator.Reservation;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -19,6 +20,9 @@ import org.json.simple.parser.ParseException;
 
 import java.util.ArrayList;
 import java.math.BigInteger;
+
+import relations.objects.LinearCombination;
+import relations.objects.R1CSConstraint;
 
 /**
  * Class that implements all necessary functions to import and load an R1CS in JSON format
@@ -32,6 +36,46 @@ public class JSONR1CSLoader {
         this.filename = jsonFile;
     };
 
+    private <FieldT extends AbstractFieldElementExpanded<FieldT>>
+    LinearCombination<FieldT> loadLinearCombination(FieldT fieldONE, JSONArray linearComb){
+        LinearCombination<FieldT> res = new LinearCombination<FieldT>();
+        for (int j = 0; j < linearComb.size(); j++) {
+            JSONObject jsonTerm = (JSONObject) linearComb.get(j);
+            String valueStr = (String) jsonTerm.get("value");
+            // Wire values are exported as hexadecimal strings
+            // (we remove the '0x' prefix using `substring`)
+            BigInteger value = new BigInteger(valueStr.substring(2), 16);
+            // FieldT extends `AbstractFieldElementExpanded` which has `construct` from BigInteger
+            FieldT valueField = fieldONE.construct(value);
+            res.add(new LinearTerm<FieldT>((Long) jsonTerm.get("index"), valueField));
+        }
+
+        return res;
+    }
+
+    private <FieldT extends AbstractFieldElementExpanded<FieldT>>
+    R1CSConstraint<FieldT> loadConstraint(FieldT fieldONE, JSONObject jsonConstraint){
+        JSONObject jsonLinCombs = (JSONObject) jsonConstraint.get("linear_combination");
+    
+        // Load `linear_combination.A` in memory
+        JSONArray jsonLinCombA = (JSONArray) jsonLinCombs.get("A");
+        LinearCombination<FieldT> linearCombA = loadLinearCombination(fieldONE, jsonLinCombA);
+        System.out.println("linearCombA = " + linearCombA.toString());
+    
+        // Load `linear_combination.B` in memory
+        JSONArray jsonLinCombB = (JSONArray) jsonLinCombs.get("B");
+        LinearCombination<FieldT> linearCombB = loadLinearCombination(fieldONE, jsonLinCombB);
+        System.out.println("linearCombB = " + linearCombB.toString());
+    
+        // Load `linear_combination.C` in memory
+        JSONArray jsonLinCombC = (JSONArray) jsonLinCombs.get("C");
+        LinearCombination<FieldT> linearCombC = loadLinearCombination(fieldONE, jsonLinCombC);
+        System.out.println("linearCombC = " + linearCombC.toString());
+    
+        R1CSConstraint<FieldT> res = new R1CSConstraint<FieldT>(linearCombA, linearCombB, linearCombC);
+        return res;
+    }
+
     // Loads the file to a "local" (i.e. non-distributed) R1CS instance
     // Need to pass `fieldONE` as a was to bypass the limitations of java generics.
     // The `construct` function is used to instantiate elements of type FieldT from `fieldONE`
@@ -42,42 +86,23 @@ public class JSONR1CSLoader {
          
         try (FileReader reader = new FileReader(this.filename)) {
             Object obj = jsonParser.parse(reader);
- 
             JSONObject jsonR1CS = (JSONObject) obj;
 
             // TODO: Retrieve the field characteristic for type safety
             // Once recovered, we can assert that r = FieldT::r to make sure types match
             // Long fieldChar = (Long) jsonR1CS.get("scalar_field_characteristic");
+            // assert (fieldChar == FieldT::mod);
 
-            JSONArray jsonConstraints = (JSONArray) jsonR1CS.get("constraints");
-            System.out.println(jsonConstraints); // DEBUG ONLY - to remove
-            for (int i = 0; i < jsonConstraints.size(); i++) {
-                JSONObject constraint = (JSONObject) jsonConstraints.get(i);
-                JSONObject linCombs = (JSONObject) constraint.get("linear_combination");
-                JSONArray linCombA = (JSONArray) linCombs.get("A");
-                ArrayList<LinearTerm<FieldT>> termsA = new ArrayList<LinearTerm<FieldT>>();
-                for (int j = 0; j < linCombA.size(); j++) {
-                    JSONObject jsonTerm = (JSONObject) linCombA.get(j);
-                    String valueStr = (String) jsonTerm.get("value");
-                    // Wire values are exported as hexadecimal strings
-                    // (we remove the '0x' prefix using `substring`)
-                    BigInteger value = new BigInteger(valueStr.substring(2), 16);
-                    System.out.println("value = " + value.toString());
-                    // FieldT is restricted to extend `AbstractFieldElementExpanded`
-                    // which has a constructor from BigInteger
-                    FieldT valueField = fieldONE.construct(value);
-                    termsA.add(new LinearTerm<FieldT>((Long) jsonTerm.get("index"), valueField));
-                }
-                System.out.println("termsA = " + termsA.toString());
-                JSONArray linCombB = (JSONArray) linCombs.get("B");
-                System.out.println("linCombB = " + linCombB.toString());
-                JSONArray linCombC = (JSONArray) linCombs.get("C");
-                System.out.println("linCombC = " + linCombC.toString());
+            JSONArray jsonConstraintArray = (JSONArray) jsonR1CS.get("constraints");
+            R1CSConstraints<FieldT> constraintArray =  new R1CSConstraints<FieldT>();
+            for (int i = 0; i < jsonConstraintArray.size(); i++) {
+                R1CSConstraint<FieldT> constraint = loadConstraint(fieldONE, (JSONObject) jsonConstraintArray.get(i));
+                constraintArray.add(constraint);
             }
-            //R1CSConstraints<FieldT> constrainsts;
 
-            Long numInputs = (Long) jsonR1CS.get("num_inputs");
-            System.out.println(numInputs);
+            //Long numInputs = (Long) jsonR1CS.get("num_inputs");
+            // TODO: Compute numAuxiliay inputs
+            //R1CSRelation<FieldT> relation = new R1CSRelation<FieldT>(constraintArray, numInputs, 42/*numAuxiliary*/);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
