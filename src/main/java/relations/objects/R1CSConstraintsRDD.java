d98fdc04ea868af3741c0da1451ce23b64f68df2
Duncan Tebbs <duncan.tebbs@gmail.com>
2021-02-24 13:23:19 +0000

    comment clarification and better naming in R1CSConstraintsRDD

--- src/main/java/relations/objects/R1CSConstraintsRDD.java
+++ src/main/java/relations/objects/R1CSConstraintsRDD.java
@@ -19,44 +19,55 @@ import org.apache.spark.api.java.JavaPairRDD;
  * <p>In other words, the system is satisfied if and only if there exist a USCS (Unitary-Square
  * Constraint System) variable assignment for which each R1CSRelation constraint is satisfied.
  *
- * <p>NOTE: The 0-th variable (i.e., "x_{0}") always represents the constant 1 Thus, the 0-th
+ * <p>NOTE: The 0-th variable (i.e., "x_{0}") always represents the constant 1. Thus, the 0-th
  * variable is not included in num_variables.
  */
 public class R1CSConstraintsRDD<FieldT extends AbstractFieldElementExpanded<FieldT>>
     implements Serializable {
 
-  // Linear combinations represent additive sub-circuits
-  // In other words, each LinearCombination can be represented as a dot product between X (the
-  // "wire" vector)
-  // and another vector "selecting each wires" to "route" them.
+  // Linear combinations represent additive sub-circuits.  In other words, each
+  // LinearCombination can be represented as a dot product of X (the "wire"
+  // vector) and another vector "selecting each wires" to "route" them.
   //
-  // An R1CS is a set of 3 matrices A,B,C that are represented here by a set of 3 sets of
-  // linear combinations.
+  // An R1CS is a set of 3 matrices A,B,C that are represented here by 3 sets
+  // of linear combinations.
   //
-  // NOTE: `R1CSConstraintsRDD` is handled differently than "normal"/non-distributed R1CS which is a
-  // set of "constraints" each of which being a triple of linear combinations.
-  // In fact, here the linear combinations are represented using "lists" of Linear Terms. The `Long`
-  // key of the tuple/pair `Tuple2<Long, LinearTerm<FieldT>>` (which is "implied" by the
-  // `JavaPairRDD`) represents the "index" of the linear combination of interest.
-  // For instance, the 1*a+2*b+5*d represented by <1,2,0,5> . <a,b,c,d>, leading to the ith matrix
-  // line [1 2 0 5] in the R1CS can be represented by:
-  // < Tuple2<Long, LinearTerm<Field>>(i, LinearTerm(0, 1)), Tuple2<Long, LinearTerm<Field>>(i,
-  // LinearTerm(1, 2)), Tuple2<Long, LinearTerm<Field>>(i, LinearTerm(3, 0)), Tuple2<Long,
-  // LinearTerm<Field>>(i, LinearTerm(4, 5)) >
+  // Unlike the regular R1CSConstraints, the distributed constraints are stored
+  // as flattened collections (RDDs) of pairs:
+  //
+  //   (constraint_idx, LinearTerm(var_idx, coefficient))
+  //
+  // with a collection for each of the A, B and C inputs. Multiple entries may
+  // appear in each collection for a single constraint (where one of the inputs
+  // is a linear combination involving multiple terms). For example, given the
+  // inputs or "wire" vector (x0, x1, x2, ...), the i-th constraint:
+  //
+  //   (2 + 3*x1 + x4) * (7*x2 + x5) = (4 * x3)
+  //
+  // would appear as the following entries in A, B and C:
+  //
+  //            A                        B                        C
+  //           ~~~                      ~~~                      ~~~
+  //   ...                      ...                      ...
+  //   (i, LinearTerm(0, 2))    (i, LinearTerm(2, 7))    (i, LinearTerm(0, 4))
+  //   (i, LinearTerm(1, 3))    (i, LinearTerm(5, 1))    (i, LinearTerm(3, 1))
+  //   (i, LinearTerm(4, 1))    ...                      ...
+  //   ...                      ...                      ...
+
   private JavaPairRDD<Long, LinearTerm<FieldT>> A;
   private JavaPairRDD<Long, LinearTerm<FieldT>> B;
   private JavaPairRDD<Long, LinearTerm<FieldT>> C;
-  private long constraintSize;
+  private long numConstraints;
 
   public R1CSConstraintsRDD(
       final JavaPairRDD<Long, LinearTerm<FieldT>> _A,
       final JavaPairRDD<Long, LinearTerm<FieldT>> _B,
       final JavaPairRDD<Long, LinearTerm<FieldT>> _C,
-      final long _constraintSize) {
+      final long _numConstraints) {
     A = _A;
     B = _B;
     C = _C;
-    constraintSize = _constraintSize;
+    numConstraints = _numConstraints;
   }
 
   public JavaPairRDD<Long, LinearTerm<FieldT>> A() {
@@ -72,7 +83,7 @@ public class R1CSConstraintsRDD<FieldT extends AbstractFieldElementExpanded<Fiel
   }
 
   public long size() {
-    return constraintSize;
+    return numConstraints;
   }
 
   public void union(R1CSConstraintsRDD<FieldT> inputRDD) {
