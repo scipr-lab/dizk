c9e4414740849f9a2a11cd55f528327a83f66c9c
Duncan Tebbs <duncan.tebbs@gmail.com>
2021-03-11 09:46:11 +0000

    util functions in BinaryCurveReader

--- src/main/java/io/BinaryCurveReader.java
+++ src/main/java/io/BinaryCurveReader.java
@@ -8,6 +8,8 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.function.Supplier;
 
 /**
  * Base class for binary readers. Handles reading fixed-width BigInteger values (in big-endian
@@ -17,24 +19,52 @@ public abstract class BinaryCurveReader<
         FrT extends AbstractFieldElementExpanded<FrT>,
         G1T extends AbstractG1<G1T>,
         G2T extends AbstractG2<G2T>>
-    implements AbstractCurveReader<FrT, G1T, G2T> {
-
-  private final DataInputStream inStream;
-
-  public DataInputStream getStream() {
-    return inStream;
-  }
+    extends DataInputStream implements AbstractCurveReader<FrT, G1T, G2T> {
 
   protected BinaryCurveReader(InputStream inStream_) {
-    inStream = new DataInputStream(inStream_);
+    super(inStream_);
   }
 
   protected BigInteger readBigInteger(final int numBytes) throws IOException {
-    final byte[] bytes = inStream.readNBytes(numBytes);
+    final byte[] bytes = readNBytes(numBytes);
     if (bytes.length != numBytes) {
       throw new IOException("unexpected end of stream");
     }
     return new BigInteger(bytes);
+  }
+
+  public int readIntLE() throws IOException {
+    final int vBE = readInt();
+    return (vBE << 24) | ((vBE << 8) & 0x00ff0000) | ((vBE >> 8) & 0x0000ff00) | (vBE >> 24);
+  }
+
+  public long readLongLE() throws IOException {
+    final long iL = (long) readIntLE();
+    final long iH = (long) readIntLE();
+    return (iH << 32) | iL;
+  }
+
+  public <T> ArrayList<T> readArrayList(Supplier<T> reader) throws IOException {
+    final long size = readLongLE();
+    ArrayList<T> elements = new ArrayList<T>(Math.toIntExact(size));
+    for (long i = 0; i < size; ++i) {
+      // It seems exceptions are not supported through Function / Supplier,
+      // etc. Workaround using nulls.
+      T value = reader.get();
+      if (value == null) {
+        throw new IOException("failed to read element");
+      }
+      elements.add(value);
+    }
+    return elements;
+  }
+
+  public <T> ArrayList<T> readArrayListNoThrow(Supplier<T> reader) {
+    try {
+      return readArrayList(reader);
+    } catch (IOException e) {
+      return null;
+    }
   }
 
   protected static <FieldT extends AbstractFieldElement<FieldT>> int computeSizeBytes(FieldT one) {
